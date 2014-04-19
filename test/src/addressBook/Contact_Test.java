dcb225677702f7f0658af1fac0e4f716ff0f1fb9
Ryan Nathaniel Smith <prilishlucnasamota@gmail.com>
2014-04-19 17:11:21 -0500

    Contact tests simplified so as not to test strategies, only that a file was created. Integration tests should test whether strategies are effective.

--- test/src/addressBook/Contact_Test.java
+++ test/src/addressBook/Contact_Test.java
@@ -1,13 +1,26 @@
 package addressBook;
 
+import static addressBook.Contact.XML;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
 import junit.framework.TestCase;
 
 public class Contact_Test extends TestCase
 {
+   private static final String BOB = "Bob Wilson";
+   private static final String GROOVY = "123 Groovy St.";
+   private static final String AUSTIN = "Austin";
+   private static final String TX = "TX";
+   private static final String AUSTIN_ZIP = "78755";
+   
+   private static final String JANE = "Jane Baker";
+   private static final String MAPLE = "5743 Maple Ave";
+   private static final String DENVER = "Denver";
+   private static final String CO = "CO";
+   private static final String DENVER_ZIP = "80294";
+   
+   private static final String XML_FILE = "R\\output\\test\\contactOne.xml";
+   private static final String CONTACT_ONE = "test\\contactOne";
+   
    private Contact contactOne;
    private Contact contactTwo;
    private Contact contactThree;
@@ -15,106 +28,32 @@ public class Contact_Test extends TestCase
    
    public void setUp()
    {
-      this.contactOne = new Contact("Bob Wilson", "123 Groovy St.", "Austin", "TX", "78755", "xml");
-      this.contactTwo = new Contact("Bob Wilson", "123 Groovy St.", "Austin", "TX", "78755", "xml");
-      this.contactThree = new Contact("Jane Baker", "123 Groovy St.", "Denver", "CO", "80294", "json");
-      this.contactFour = new Contact("Jill Park", "5743 Maple Ave", "Tucson", "AZ", "85726", "plain");
+      this.contactOne = new Contact(BOB, GROOVY, AUSTIN, TX, AUSTIN_ZIP, XML);
+      this.contactTwo = new Contact(BOB, GROOVY, AUSTIN, TX, AUSTIN_ZIP, XML);
+      this.contactThree = new Contact(JANE, GROOVY, DENVER, CO, DENVER_ZIP);
+      this.contactFour = new Contact(BOB, MAPLE, AUSTIN, TX, AUSTIN_ZIP);
    }
    
    public void testEqualsEqual()
    {
-      assertTrue("Error in testEqualsEqual: Objects are identical", this.contactOne.equals(this.contactTwo));
+      assertTrue("Error in testEqualsEqual: Objects are identical", contactOne.equals(contactTwo));
    }
    
    public void testEqualsPartiallyEqual()
    {
-      assertTrue("Error in testEqualsPartiallyEqual: Objects are not identical", !this.contactOne.equals(this.contactThree));
+      assertTrue("Error in testEqualsPartiallyEqual: Objects are not identical", !contactOne.equals(contactThree));
    }
    
    public void testEqualsNotEqual()
    {
-      assertTrue("Error in testEqualsNotEqual: Objects are completely different", !this.contactOne.equals(this.contactFour));
+      assertTrue("Error in testEqualsNotEqual: Objects are completely different", !contactOne.equals(contactFour));
    }
    
-   public void testWriteXML()
-   {
-      File f = new File("R\\output\\contactOne.xml");
-      String correct = "";
-      String actual = "";
-      if(f.exists())
-      {
-         f.delete();
-      }
-      File readFrom = new File("R\\input\\contactOne.xml");
-      try
-      {
-         Scanner readIn = new Scanner(readFrom);
-         readIn.useDelimiter("\\Z");
-         correct = readIn.next();
-      }
-      catch(FileNotFoundException fnfe)
-      {
-         fnfe.printStackTrace();
-      }
-      
-      contactOne.writeToFile("contactOne");
-      
-      f = new File("R\\output\\contactOne.xml");
-      try
-      {
-         Scanner testIn = new Scanner(f);
-         testIn.useDelimiter("\\Z");
-         actual = testIn.next();
-      }
-      catch(FileNotFoundException fnfe)
-      {
-         fnfe.printStackTrace();
-      }
-      
-      assertTrue("Error in testWriteXML: incorrect string returned.", correct.equals(actual));
-   }
-   
-   public void testWriteJSON()
-   {
-      File f = new File("R\\output\\contactThree.json");
-      String correct = "";
-      String actual = "";
-      if(f.exists())
-      {
-         f.delete();
-      }
-      File readFrom = new File("R\\input\\contactThree.json");
-      try
-      {
-         Scanner readIn = new Scanner(readFrom);
-         readIn.useDelimiter("\\Z");
-         correct = readIn.next();
-      }
-      catch(FileNotFoundException fnfe)
-      {
-         fnfe.printStackTrace();
-      }
-      
-      contactThree.writeToFile("contactThree");
-      
-      f = new File("R\\output\\contactThree.json");
-      try
-      {
-         Scanner testIn = new Scanner(f);
-         testIn.useDelimiter("\\Z");
-         actual = testIn.next();
-      }
-      catch(FileNotFoundException fnfe)
-      {
-         fnfe.printStackTrace();
-      }
-      
-      System.out.println("Correct: " + correct);
-      System.out.println("Actual: " + actual);
-      assertTrue("Error in testWriteJSON: incorrect string returned.", correct.equals(actual));   }
-   
-   public void testWritePlainText()
-   {
-      contactFour.writeToFile("contactFour");
+   public void testWriteToFile()
+   { 
+      contactOne.writeToFile(CONTACT_ONE);
+      File f = new File(XML_FILE);
+      assertTrue("Error in testWriteToFile: File does not exist", f.exists());
+      f.delete();
    }
 }
